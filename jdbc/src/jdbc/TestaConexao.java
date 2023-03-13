80b07232996548e98be448737d664a0b74bc3988
bianca275 <biaires27@outlook.com>
2023-03-13 20:14:01 -0300

    Aula 2 - Parte 2

--- jdbc/src/jdbc/TestaConexao.java
+++ jdbc/src/jdbc/TestaConexao.java
@@ -1,15 +1,14 @@
 package jdbc;
 
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class TestaConexao {
 
 	public static void main(String[] args) throws SQLException {
 		
-		Connection con = DriverManager
-				.getConnection("jdbc:mysql://localhost/loja_virtual?useTimezone=true&serverTimezone=UTC", "localhost", "localhost27#");
+		ConnectionFactory connectionFactory = new ConnectionFactory();
+		Connection con = connectionFactory.recuperarConexao();
 		
 		System.out.println("Fechando conexão!");
 		con.close();
