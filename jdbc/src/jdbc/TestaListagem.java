bef2f481912a7c850fc56e6a42d680f35b3d7d85
bianca275 <biaires27@outlook.com>
2023-03-14 23:51:38 -0300

    Aula 3 - Parte 2 - Ajustando as classes para PreparedStatement - Remocao de valores fixos da query

--- jdbc/src/jdbc/TestaListagem.java
+++ jdbc/src/jdbc/TestaListagem.java
@@ -1,9 +1,9 @@
 package jdbc;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
 
 public class TestaListagem {
 	
@@ -12,8 +12,9 @@ public class TestaListagem {
 		ConnectionFactory connectionFactory = new ConnectionFactory();
 		Connection con = connectionFactory.recuperarConexao();
 		
-		Statement stm = con.createStatement();
-		stm.execute("SELECT ID, NOME, DESCRICAO FROM PRODUTO");
+		PreparedStatement stm = con.prepareStatement("SELECT ID, NOME, DESCRICAO FROM PRODUTO");
+		
+		stm.execute();
 		
 		ResultSet rst = stm.getResultSet();
 		
--- jdbc/src/jdbc/TestaRemocao.java
+++ jdbc/src/jdbc/TestaRemocao.java
@@ -1,20 +1,22 @@
 package jdbc;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
-import java.sql.Statement;
 
 public class TestaRemocao {
 
 	public static void main(String[] args) throws SQLException {
 		ConnectionFactory factory = new ConnectionFactory();
 		Connection con = factory.recuperarConexao();
-		
-		Statement stm = con.createStatement();
-		stm.execute("DELETE FROM PRODUTO WHERE ID > 2");
-		
+
+		PreparedStatement stm = con.prepareStatement("DELETE FROM PRODUTO WHERE ID > ?");
+		stm.setInt(1, 5);
+
+		stm.execute();
+
 		Integer linhasModificadas = stm.getUpdateCount();
-		
+
 		System.out.println("Quantidade de linhas que foram modificadas: " + linhasModificadas);
 	}
 
